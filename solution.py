20a30655d2fc4bc371d734d391fd2276034a813c
ryzhval <130060842+ryzhval@users.noreply.github.com>
2023-04-07 10:45:33 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 
 from scipy.stats import norm
+from scipy.stats import chi2
 
 
 chat_id = 371649437 # Ваш chat ID, не меняйте название переменной
@@ -10,11 +11,12 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    alpha = 1 - p
+    #alpha = 1 - p
 #     loc = x.mean()
 #     scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
 #     return loc - scale * norm.ppf(1 - alpha / 2), \
 #            loc - scale * norm.ppf(alpha / 2)
-    return x.mean() - np.sqrt(np.var(x)) * norm.ppf(1 - alpha / 2) / np.sqrt(len(x)), \
-           x.mean() - np.sqrt(np.var(x)) * norm.ppf(alpha / 2) / np.sqrt(len(x))
+    n = len(x)
+    return np.sqrt(sum(x**2) / (30*chi2.ppf((1+p)/2, df = 2*n))), \
+           np.sqrt(sum(x**2) / (30*chi2.ppf((1-p)/2, df = 2*n)))
     
